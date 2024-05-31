f7e35f17087ef0b7a30b92deeb0bacc6197f8900
eskayML <kalusam619@gmail.com>
2024-05-31 23:11:54 +0100

    sdf

--- NN.py
+++ NN.py
@@ -25,13 +25,7 @@ class Model(nn.Module):
         self.num_classes = num_classes
         self.stack = nn.Sequential(
             nn.Dropout(p = self.dp),
-            nn.Linear(in_size, self.width),
-            self.act,
-            nn.Dropout(p = self.dp),
-            nn.Linear(in_size, self.width),
-            self.act,
-            nn.Dropout(p = self.dp),
-            nn.Linear(in_size, self.width),
+            nn.Linear(self.width, self.width),
             self.act,
             nn.Dropout(p = self.dp),
             nn.Linear(self.width, self.width),
